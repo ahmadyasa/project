9c68dab5005dc8b7b8a92ba5f24283797ecc9d9f
ahmadyasa <lanayasa95@gmail.com>
2026-01-14 21:10:54 +0700

    tambah

--- resources/views/lihatpost.blade.php
+++ resources/views/lihatpost.blade.php
@@ -50,7 +50,7 @@
                 + Tambah Kategori
             </a>
         </div>
-        
+
         <!-- Alert -->
         @if (session('success'))
             <div class="bg-green-100 text-green-700 p-3 rounded mb-4">
